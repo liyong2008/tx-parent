f288212ff5b9d8c6048bb9baab1036842cfa6d32
Administrator <Administrator@USER-20150902AZ>
2019-01-28 23:18:33 +0800

    commit and push

--- tx-components/tx-component-starter-basicdata/src/main/java/com/tx/component/configdict/service/ConfigPropertyService.java
+++ tx-components/tx-component-starter-basicdata/src/main/java/com/tx/component/configdict/service/ConfigPropertyService.java
@@ -16,6 +16,6 @@ package com.tx.component.configdict.service;
  * @see  [相关类/方法]
  * @since  [产品/模块版本]
  */
-public interface ConfigPropertyPersister {
+public interface ConfigPropertyService {
     
 }
